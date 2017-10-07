39b585835801a9edfc7d492b7d591ed92ce37920
Josh Frankel <joshmfrankel@gmail.com>
2017-10-07 19:54:24 -0400

    Add player death handler, Random enemy loader

--- js/components/enemy.js
+++ js/components/enemy.js
@@ -3,28 +3,39 @@ class UnimplementedMethodError extends Error {}
 class Enemy extends Phaser.Sprite {
   constructor(game, x, y, sprite) {
     super(game);
+    this.game = game;
 
     // Build the sprite based on the string from the constructor
     Phaser.Sprite.call(this, game, x, y, sprite);
 
+    this.defaults();
+
     // Hook method for overriding default functionality
     this.postConfigurationHook();
-
-    // Default enemy settings
-    game.physics.arcade.enable(this);
-    this.body.collideWorldBounds = true;
   }
 
   postConfigurationHook() {
     throw new UnimplementedMethodError("must implement method in subclasses");
   }
+
+  // Default enemy settings
+  defaults () {
+    this.game.physics.arcade.enable(this);
+    this.body.collideWorldBounds = true;
+  }
 }
 
+/**
+ * Drone
+ *
+ * The most basic enemy fighter
+ */
 class Drone extends Enemy {
   postConfigurationHook() {
     this.configuration = {
       points: 10,
-      health: 1
+      health: 1,
+      speed: 150
     }
   }
 }
